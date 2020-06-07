709ff93c05e22175d304dbdee8b3d1bf894381ff
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2020-06-07 13:55:49 -0300

    Configuração inicial do projeto

---
+++ src/main/java/br/com/priscila/zedelivery/ZeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.priscila.zedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/priscila/zedelivery/ZeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.priscila.zedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
