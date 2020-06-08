c68e0c46f37402f409efe1cdae405d3920782727
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2020-06-07 23:06:38 -0300

    adiciona camada de service

--- src/main/java/br/com/priscila/zedelivery/services/PdvService.java
+++ src/main/java/br/com/priscila/zedelivery/services/PdvService.java
@@ -1,4 +1,26 @@
 package br.com.priscila.zedelivery.services;
 
+import br.com.priscila.zedelivery.model.Pdv;
+import br.com.priscila.zedelivery.repository.PdvRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PdvService {
+
+    @Autowired
+    private PdvRepository repository;
+
+    public List<Pdv> findAll(){
+        return repository.findAll();
+    }
+
+    public Pdv findById(Long id){
+        Optional<Pdv> obj = repository.findById(id);
+        return obj.get();
+    }
+
 }
